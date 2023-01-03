1c4ca00d958d76913c77f2a2f9e708ea8f888320
Columbiysky <c.7843543@gmail.com>
2023-01-03 12:47:24 +0300

    Update MainPageView.vue

--- Client/app/src/views/MainPageView.vue
+++ Client/app/src/views/MainPageView.vue
@@ -10,26 +10,60 @@
         <InputText id="link" type="text" v-model="currentLink" />
       </div>
       <div class="item" style="padding-left: 3px;">
-        Minimal timeout (Minutes):
+        Timeout in Minutes:
         <InputNumber v-model="currentTimeoutInMinutes" :min="60" />
       </div>
     </div>
-    <div style="background-color: green;">green</div>
+    <div style="padding-top: 5px;">
+      <DataTable :value="data">
+        <Column field="name" header="Name"></Column>
+        <Column field="link" header="Link"></Column>
+        <Column field="timeout" header="Timeout"></Column>
+        <Column field="timeOfLastCheck" header="Time of last check"></Column>
+        <Column>
+          <template #body>
+            <Button type="button" icon="pi pi-pencil"></Button>
+          </template>
+        </Column>
+      </DataTable>
+    </div>
   </div>
 </template>
 
 <script>
 /* eslint-disable */
 const minimalTimeoutInMinutes = 60
-
+const defaultOptions = {
+  year: 'numeric',
+  month: 'numeric',
+  day: 'numeric',
+  hour: 'numeric',
+  minute: 'numeric',
+  hour12: false,
+  timeZone: 'Europe/Moscow',
+}
 export default {
   data() {
     return {
-      data: {},
+      data: null,
+      options: defaultOptions,
       currentLink: '',
       currentName: '',
       currentTimeoutInMinutes: minimalTimeoutInMinutes,
     }
+  },
+  created() {
+    this.data = [
+      {
+        name: 'Кронштейн под моники',
+        link:
+          'https://www.dns-shop.ru/product/88150834b7af2eb1/kreplenie-dla-monitorov-nb-f160/',
+        timeout: 60,
+        timeOfLastCheck: new Intl.DateTimeFormat('ru-RU', this.options).format(
+          new Date(),
+        ),
+      },
+    ]
   },
   name: 'MainPage',
   props: {},
