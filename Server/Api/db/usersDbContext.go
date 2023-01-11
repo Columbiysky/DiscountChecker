d9aa12bee01ed8b2d5d84d779e37be9664a38c17
Columbiysky <c.7843543@gmail.com>
2023-01-11 23:06:46 +0300

    conn string changed users db context register new user func added

--- Server/Api/db/usersDbContext.go
+++ Server/Api/db/usersDbContext.go
@@ -9,6 +9,8 @@ import (
 type User struct {
 	Id              int
 	Username        string
+	UserSettingsId  int
+	Password        string
 	Token           string
 	TokenExpiration time.Time
 }
@@ -36,6 +38,29 @@ func getUserById(id int) User {
 	row, err := db.Query("select * from Users where Id=$1", id)
 	return getUserInstanceFromRow(row)
 }
+
+func registerNewUser(newUser User) {
+	var dbConfig = getConfig()
+	db, err := sql.Open("postgres", dbConfig.ConnectionString)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	result, err := db.Exec("insert into Users (UserName, Password, UserSettingsId) values ($1, $2, $3)",
+		newUser.Username, newUser.Password, -1)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(result.LastInsertId()) // не поддерживается
+	fmt.Println(result.RowsAffected()) // количество добавленных строк
+}
+
+//update example
+// result, err := db.Exec("update Products set price = $1 where id = $2", 69000, 1)
+//     if err != nil{
+//         panic(err)
+//     }
 
 // returns True if token expired
 func checkTokenExpirationById(id int) bool {
